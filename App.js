2536dbcf0e632fb9b2e12d7e53f29fb422c4c0ad
Amina7744 <aminazabeirououmarou192@gmail.com>
2025-02-11 15:58:44 +0100

    J'ai créer un nouvau projet parce que j'ai rencontré qes probles dans l'ancien

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import NoteDetailScreen from './src/screens/NoteDetailScreen';
+import AddNoteScreen from './src/screens/AddNoteScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#4CAF50",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                title: "Les notes"
+              }}
+            />
+            <Stack.Screen
+            name="AddNote"
+            component={AddNoteScreen}
+            options={{
+              title: "Ajout de notes"
+            }}
+            />
+            <Stack.Screen
+            name="NoteDetail"
+            component={NoteDetailScreen}
+            options={{
+              title: "Details sur les notes"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ }
